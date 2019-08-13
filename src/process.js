2f9c9466dd8297a0c78043d9643435f22f0b53c7
speed-sonic <blldxt@gmail.com>
2019-08-13 23:11:34 +0800

    base code

--- src/process.js
+++ src/process.js
@@ -223,4 +223,21 @@
 
     elementController.ontouchstart = function(event) {event.preventDefault()};
     elementMain.ontouchstart = function(event) {event.preventDefault()};
+
+    
+    function isPhone() {
+        var userAgentInfo = navigator.userAgent;
+        var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+        var flag = false;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+    if (!isPhone()) {
+        alert('请使用手机打开游戏哦~');
+    }
 })();
